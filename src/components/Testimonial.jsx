68af06e846a0ab401d5453c3385ed59976e4894d
Dixon G <dxxn0101@gmail.com>
2023-03-28 17:45:19 -0600

    fix margin of testimonial section

--- src/components/Testimonial.jsx
+++ src/components/Testimonial.jsx
@@ -3,7 +3,7 @@ import { TestimonialSlider } from "./TestimonialSlider";
 
 export const Testimonial = () => {
   return (
-    <section className="mb-64 lg:mb-48">
+    <section className="mb-80 sm:mb-64 lg:mb-48">
       <div className="container mx-auto">
         <div className="bg-purple-400/10 max-h-[400px] p-12 rounded-lg">
           <div className="text-center">
